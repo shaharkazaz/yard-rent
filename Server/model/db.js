59d5c578d40211da855fbda48bf3ffe10c7c900e
unknown <chenamiell@gmail.com>
2020-01-04 15:44:30 +0200

    Server: Express Skeleton + Mongoose
    1) init Express Skeleton
    2) init Mongoose

---
+++ Server/model/db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://uy9uxa5ensvbjwshje2a:KzfvYuOnKVCaQrZ26jpJ@bytcreskhtan8ld-mongodb.services.clever-cloud.com:27017/bytcreskhtan8ld', { useNewUrlParser:true, useUnifiedTopology: true });
