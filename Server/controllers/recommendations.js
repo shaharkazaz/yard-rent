8d27cf04687a727fe077505220a0747bf2dd1925
Chen Amiel <chenamiell@gmail.com>
2020-03-06 18:19:35 +0200

    Server | content base recommender

--- Server/controllers/recommendations.js
+++ Server/controllers/recommendations.js
@@ -1,44 +1,58 @@
 const mongoose = require('mongoose');
 const Product = require('../model/product');
-const User = require('../model/user');
-const Order = require('../model/order');
-const ContentBasedRecommender = require('content-based-recommender')
+const Recommendation = require('../model/recommendation');
 
+const ContentBasedRecommender = require('content-based-recommender')
+const RecommendationProductsNumber = 3;
 const recommender = new ContentBasedRecommender({
     minScore: 0,
-    maxSimilarDocuments: 1000,
+    maxSimilarDocuments: 100,
     debug: true
 });
+const formatData = data => {
+    let formatted = [];
+    for (const [key,labels] of Object.entries(data)) {
+        let tmpObj = {
+            id: labels._id,
+            content: [labels.name,labels.category.name,labels.subCategory.subCategoryName,labels.rewards,labels.deposit].join(" ")
+        };
 
+        formatted.push(tmpObj);
+    }
+
+    return formatted;
+};
 module.exports = {
     getRecommendation: (req, res) => {
-
-        const documents = [
-            { id: '1000001', content: {
-                    name: "prom",
-                    user: "chen@gmail.com"
-                }},
-            { id: '1000002', content: {
-                    name: "fish",
-                    user: "chen@gmail.com"
-                } },
-            { id: '1000003', content: {
-                    name: "shoes",
-                    user: "ayelet@gmail.com"
-                } },
-            { id: '1000004', content: {
-                    name: "dress",
-                    user: "ayelet@gmail.com"
-                } }
-        ];
-
-        recommender.train(documents);
-
-        const similarDocuments = recommender.getSimilarDocuments('1000002', 0, 1);
-
-        res.status(200).json({
-            similarDocuments
+        const productId = req.params.productId;
+        Product.find({}, {
+            name: 1,
+            category: 1,
+            subCategory: 1,
+            rewards: 1,
+            deposit: 1
+        }).populate('category').populate('subCategory').then((products) => {
+            const result = formatData(products);
+            recommender.train(result);
+            const similarDocuments = recommender.getSimilarDocuments(productId, 0, RecommendationProductsNumber);
+            const ids = similarDocuments.map(l=>l.id);
+            const recommendation = new Recommendation({
+                _id: mongoose.Types.ObjectId(productId),
+                recommendedProducts: ids
+            });
+            recommendation.save().then(() => {
+                res.status(200).json({
+                    similarDocuments
+                })
+            }).catch(error => {
+                return res.status(500).json({
+                    error
+                })
+            });
+        }).catch(error => {
+            res.status(500).json({
+                error
+            })
         })
-
-        },
-    };
+    }
+};
