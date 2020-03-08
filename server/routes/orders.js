3ff4da16207f32331e928f829a7544b0994531bf
unknown <chenamiell@gmail.com>
2020-03-08 20:46:40 +0200

    Server | fix authorization on routes

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { role } = require('../utils/role');
 
 const {getAllOrders, addOrder, getOrder, updateOrder, deleteOrder} = require('../controllers/orders');
 const checkAuth = require('../middlewares/checkAuth');
 
-router.get('/', getAllOrders);
-router.get('/:orderId', getOrder);
+router.get('/', checkAuth(role.Admin) , getAllOrders);
+router.get('/:orderId', checkAuth() , getOrder);
 router.post('/', checkAuth(), addOrder);
-router.patch('/:orderId', checkAuth(), updateOrder);
-router.delete('/:orderId', checkAuth(), deleteOrder);
+router.patch('/:orderId', checkAuth(role.Admin), updateOrder);
+router.delete('/:orderId', checkAuth(role.Admin), deleteOrder);
 
 
 module.exports = router;
--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { role } = require('../utils/role');
 
 const { getAllShops, addShop, getShop, updateShop, deleteShop } = require('../controllers/shops');
 const checkAuth = require('../middlewares/checkAuth');
 
 router.get('/', getAllShops);
 router.get('/:shopId', getShop);
-router.post('/', checkAuth(), addShop);
-router.patch('/:shopId', checkAuth(), updateShop);
-router.delete('/:shopId', checkAuth(), deleteShop);
+router.post('/', checkAuth(role.Admin), addShop);
+router.patch('/:shopId', checkAuth(role.Admin), updateShop);
+router.delete('/:shopId', checkAuth(role.Admin), deleteShop);
 
 
 module.exports = router;
