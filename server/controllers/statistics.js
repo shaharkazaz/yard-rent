83f3875e3e5168d6ee7e41d3e3992c18e119f140
unknown <chenamiell@gmail.com>
2020-03-08 22:23:08 +0200

    Server | mock static

--- server/controllers/statistics.js
+++ server/controllers/statistics.js
@@ -1,21 +1,21 @@
 module.exports = {
     graf1: (req, res) => {
         const response = [{
-            Day: "Sunday",
-            Orders: 50,
-            Rewards: 20
+            day: "Sunday",
+            orders: 50,
+            rewards: 20
         } ,{
-            Day: "Monday",
-            Orders: 30,
-            Rewards: 10
+            day: "Monday",
+            orders: 30,
+            rewards: 10
         } ,{
-            Day: "Tuesday",
-            Orders: 19,
-            Rewards: 15
+            day: "Tuesday",
+            orders: 19,
+            rewards: 15
         }, {
-            Day: "Wednesday",
-            Orders: 78,
-            Rewards: 150
+            day: "Wednesday",
+            orders: 78,
+            rewards: 150
         }]
         res.status(200).json({
             data: response
@@ -24,17 +24,17 @@ module.exports = {
     ,
     graf2: (req, res) => {
         const response = [{
-            Category: "Furnitire",
-            Orders: 50
+            category: "Furnitire",
+            orders: 50
         }, {
-            Category: "Shoes",
-            Count: 20
+            category: "Shoes",
+            count: 20
         }, {
-            Category: "Electric",
-            Count: 44
+            category: "Electric",
+            count: 44
         }, {
-            Category: "Garden",
-            Count: 12
+            category: "Garden",
+            count: 12
         }]
         res.status(200).json({
             data: response
