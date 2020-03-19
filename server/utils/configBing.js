e73d19bcf1f454d6e2937c323fb23ec6a9d20d49
Niv Ohavi <OhaviNiv@gmail.com>
2020-03-19 18:41:34 +0200

    - created GPS mechanism - added getCoordinatesByAddress function

---
+++ server/utils/configBing.js
@@ -0,0 +1,3 @@
+module.exports = {
+    apiKey: "?key=AkDnTh-F1E565FfNPYCHbjs4JRMN6fC43_WivOhXMja2MVEFmDJhO2ZY8r_MuX54"
+}
---
+++ server/utils/gpsApi.js
@@ -0,0 +1,40 @@
+const querystring = require('querystring');
+const http = require("http");
+const configBing = require('../utils/configBing');
+
+
+async function getCoordinatesByAddress(req){
+    return new Promise(resolve => {
+
+        const address = req.body.address;
+        const city = req.body.city;
+        const country = req.body.country;
+
+        // Prepare url string
+        const fullAddressSplitted = (address + city + country).split(" ")
+        let addressToSubmit = ""
+        fullAddressSplitted.forEach((i) => {
+            addressToSubmit += i;
+        })
+
+        const baseUrl = "http://dev.virtualearth.net/REST/v1/Locations/"
+        const apiKey = configBing.apiKey.toString()
+        const s = baseUrl + addressToSubmit + apiKey
+        http.get(s, res => {
+            res.setEncoding("utf8");
+            let body = "";
+
+            res.on("data", data => {
+                body += data;
+            });
+            res.on("end", () => {
+                body = JSON.parse(body);
+                const lat = body.resourceSets[0].resources[0].geocodePoints[0].coordinates[0]
+                const long = body.resourceSets[0].resources[0].geocodePoints[0].coordinates[1]
+                resolve(lat + "," + long);
+            });
+        });
+    });
+}
+
+module.exports = getCoordinatesByAddress;
