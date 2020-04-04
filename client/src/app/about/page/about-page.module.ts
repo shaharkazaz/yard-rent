30bd160840a52e3ada65561020bf599728392a38
Niv Ohavi <OhaviNiv@gmail.com>
2020-04-04 19:01:21 +0300

    feat: 🎸 added general calss and slogan translation

--- client/src/app/about/page/about-page.module.ts
+++ client/src/app/about/page/about-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './about-page.component';
 import {CommonModule} from '@angular/common';
+import {TranslocoModule} from "@ngneat/transloco";
 
 const routes: Routes = [
   {
@@ -11,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), CommonModule],
+  imports: [RouterModule.forChild(routes), CommonModule, TranslocoModule],
   declarations: [AboutPageComponent],
   exports: [AboutPageComponent]
 })
