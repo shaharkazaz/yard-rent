757d9db9452855787b983e5373f924092631963b
Niv Ohavi <OhaviNiv@gmail.com>
2020-05-31 19:45:52 +0300

    create userNewMessages route for querying messages that wasnt readin

--- server/model/message.js
+++ server/model/message.js
@@ -1,10 +1,10 @@
 
 const mongoose = require('mongoose');
 
-const notificationSchema = mongoose.Schema({
+const messageSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectID,
     order: { type: mongoose.Schema.Types.ObjectID, ref: 'Order', required: false },
     type: ["orderIsAboutToExpire24H" , "orderIsAboutToExpire48H"]
 });
 
-module.exports = mongoose.model('Notification', notificationSchema);
+module.exports = mongoose.model('Message', messageSchema);
