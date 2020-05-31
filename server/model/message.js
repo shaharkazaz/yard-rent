cef14463192aec42624832d9363a84e922269aeb
Niv Ohavi <OhaviNiv@gmail.com>
2020-05-31 22:40:53 +0300

    send user message on order return date is getting closer

--- server/model/message.js
+++ server/model/message.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectID,
+    date: {type: Date, default: Date.now(), required: true},
     order: { type: mongoose.Schema.Types.ObjectID, ref: 'Order', required: false },
     isOpened: { type: Boolean, default: false },
     type: ["orderIsAboutToExpire24H" , "orderIsAboutToExpire48H"]
