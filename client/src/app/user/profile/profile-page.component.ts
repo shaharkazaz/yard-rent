43e4f6837099c3194f1524411a9f55cfbebc6341
ayelet-ron <ayeletron161@gmail.com>
2020-08-08 16:22:28 +0300

    Client | fix bug user profile load from store

--- client/src/app/user/profile/profile-page.component.ts
+++ client/src/app/user/profile/profile-page.component.ts
@@ -1,9 +1,11 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AuthQuery} from '../../auth/state/auth.query';
-import {AppAuthService} from "../../auth/app-auth.service";
-import {UserInfo} from "../../auth/state/auth.model";
-import {formatNumber} from "../../shared/utils";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {AppAuthService} from '../../auth/app-auth.service';
+import {UserInfo} from '../../auth/state/auth.model';
+import {formatNumber} from '../../shared/utils';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {AuthService} from '../../auth/state/auth.service';
+import {untilDestroyed} from 'ngx-take-until-destroy';
 
 @Component({
   selector: 'app-profile',
@@ -14,17 +16,25 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class ProfilePageComponent implements OnInit {
   user: UserInfo;
   userProfile: FormGroup;
+  loading = true;
 
   constructor(
     private authQuery: AuthQuery,
     private fb: FormBuilder,
     private appAuthService: AppAuthService,
+    private authService: AuthService
   ) {}
 
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnDestroy(): void {}
+
   ngOnInit() {
-    this.user = this.authQuery.getValue().user;
-    this.userProfile = this.fb.group({
-      name: [this.user.name]
+    this.authService.getUserByToken().pipe(untilDestroyed(this)).subscribe(user => {
+      this.user = user;
+      this.loading = false;
+      this.userProfile = this.fb.group({
+        name: [this.user.name]
+      });
     });
   }
 
